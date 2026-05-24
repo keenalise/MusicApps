5b15722a7bc305d047947aca915dcd4826a4dccf
Keen Alise <keenalise199@gmail.com>
2026-05-24 23:32:50 +0545

    fixed bug

--- app/src/main/java/com/example/musicc/data/room/AppDatabase.kt
+++ app/src/main/java/com/example/musicc/data/room/AppDatabase.kt
@@ -13,7 +13,7 @@ import androidx.room.RoomDatabase
         PlaylistEntity::class,
         PlaylistSongEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
